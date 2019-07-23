257572759b01e3af13352446117cb8985334552a
dudu <dudu@flygresor.se>
2019-07-24 00:25:32 +0200

    untested music generation

--- data_processor.py
+++ data_processor.py
@@ -21,17 +21,17 @@ def extract_data():
 		for element in notes_and_chords: 
 			if isinstance(element, m21.note.Note): 
 				str_list.append(str(element.pitch))
-				str_list.append(str(element.duration.quarterLength))
+				str_list.append(str(element.duration.quarterLength)+'*')
 				#print(str_list[-1])
 			elif isinstance(element, m21.chord.Chord):
 				#str_list.append('.'.join(str(n) for n in element.pitches))
 				for note in element.pitches: 
 					str_list.append(str(note))
-				str_list.append(str(element.duration.quarterLength))
+				str_list.append(str(element.duration.quarterLength)+'*')
 				#print(str_list[-1])
 			elif isinstance(element,m21.note.Rest): 
 				str_list.append('rest')
-				str_list.append(element.duration.quarterLength)
+				str_list.append(str(element.duration.quarterLength)+'*')
 				#print(str_list[-1])
 		print('list length = ', len(str_list))
 	return str_list
@@ -67,3 +67,47 @@ def formate_data(processed_data, class_to_ind, seq_len, n_classes):
 
 # Given a by the model predicted list, this creates the midi file
 def create_midi(predicted_list): 
+	time_elapsed = 0 # absolute time elapsed in the generated tune
+	m21_predictions = [] # will contain music21 notes, chords and rests
+	this_chord = []
+	for i, element in enumerate(predicted_list): 
+		if '*' in element: # is a duration
+			duration = element.split('*')[0]
+			if '/' in duration: 
+				numbers = duration.split('/')
+				duration = float(numbers[0])/float(numbers[1])
+			else: 
+				duration = float(duration)
+			if len(this_chord) == 1: # it is a note or rest
+				if this_chord[0] == 'rest': 
+					rest = m21.note.Rest()
+					rest.offset = time_elapsed
+					rest.duration = duration 
+					m21_predictions.append(rest)
+				else: 
+					note = m21.note.Note(chord[0])
+					note.offset = time_elapsed
+					note.storedInstrument = instrument.Piano()
+					note.duration = duration
+					m21_predictions.append(note)
+			elif len(this_chord) > 1: # it is a chord
+				these_notes = []
+				for this_note in this_chord: 
+					if this_note == 'rest': 
+						pass
+					else: 
+						note = m21.note.Note(this_note)
+						note.storedInstrument = instrument.Piano()
+						these_notes.append(note)
+				if len(these_notes) > 0:
+					chord = m21.chord.Chord(these_notes)
+					chord.offset = time_elapsed
+					chord.duration = duration
+					m21_predictions.append(chord)
+			time_elapsed += duration
+			this_chord = []
+		else: 
+			this_chord.append(element)
+	midi_stream = m21.stream.Stream(m21_predictions)
+	midi_stream.write('midi', fp='generated_music.mid')
+
--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from model import *
 
 
 # Data preparation
-processed_data = extract_data()
-class_to_ind, ind_to_class = create_dictionaries(processed_data)
+processed_data = extract_data() # converting midi data to strings in a list
+class_to_ind, ind_to_class = create_dictionaries(processed_data) # mapping string to int and back
 
-# Parameters
+# Setting parameters
 n_classes = len(class_to_ind)
 n_data = len(processed_data)
 seq_len = 100
@@ -19,7 +19,8 @@ print(n_classes)
 eta = 1e-4 # initial learning rate
 eta_decay = 1e-6
 
-# Creating input sequences and labels for the network
+# Creating input sequences and labels for the network 
+# go from string to numerical data
 input_data, labels = formate_data(processed_data, class_to_ind, seq_len, n_classes)
 print('The shape of the input data numpy array is ', input_data.shape)
 
@@ -28,12 +29,11 @@ print('='*70)
 model = create_model(input_data, n_classes, eta, eta_decay)
 print('='*70)
 
-"""
+# """
 ### ========== Training the network ======== ###
 
-# and saving weights frequently during training
-filename = "model_at_{epoch:02d}_{loss:.4f}_bigger.hdf5" 
-
+# and saving weights after every epoch
+filename = "saved_weights/model_at_{epoch:02d}_{loss:.4f}.hdf5" 
 checkpoint = ModelCheckpoint(
 	filename, 
 	monitor = 'loss', 
@@ -43,6 +43,7 @@ checkpoint = ModelCheckpoint(
 )
 callbacks = [checkpoint]
 
+# The actual training
 model.fit(
 	input_data, 
 	labels, 
@@ -50,6 +51,7 @@ model.fit(
 	batch_size = n_batches, 
 	callbacks = callbacks
 )
+
 """
 
 ### ========== Testing the network ======== ###
@@ -57,7 +59,7 @@ model.fit(
 # Create the model with saved weights loaded 
 weights_path = 'saved_weights/model_at_05_3.7365_bigger.hdf5' # check before running
 trained_model = create_model(input_data, n_classes, eta, eta_decay)
-trained_model.load_weights(weights_path)
+trained_model.load_weights(weights_path) 
 
 # I use the first 100 datapoints as the start input for music generation
 pred_input = input_data[0, 0:seq_len, 0]
@@ -65,6 +67,7 @@ pred_input = np.reshape(pred_input, (seq_len))
 
 n_predictions = 4000 # number of notes/rests/durations to generate
 predictions = [] # holding the generated music before turned to midi formate
+
 for model_prediction in range(n_predictions): 
 	shaped_input = np.reshape(pred_input, (1, seq_len, 1))
 	prediction = trained_model.predict(shaped_input, verbose=0)
@@ -74,3 +77,6 @@ for model_prediction in range(n_predictions):
 
 	pred_input = np.append(pred_input, [note_ind/n_classes], axis = 0)
 	pred_input = pred_input[1:seq_len+1]
+print('Predictions finished. Starting convertion to midi file.')
+create_midi(predictions)
+"""
